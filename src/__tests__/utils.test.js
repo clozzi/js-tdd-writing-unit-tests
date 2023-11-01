1a442a510f71d0f59a49bbfb386bb094f9b2c7f0
Christopher Lozzi <clozzi12@gmail.com>
2023-11-01 11:19:09 -0400

    refactored

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -2,7 +2,7 @@ import { pointsForWord } from "../utils";
 
 describe("pointsForWord", () => {
     it("calculates the total points for a word (1 point per vowel, 2 per consonant)", () => {
-        const word = "test";
+        const word = "tEsT";
         const points = pointsForWord(word);
         expect(points).toBe(7);
     });
